aee1d051fd2415add9818f107d68f389c9017d7c
CodeFace <i@codeface.cn>
2017-11-26 20:25:48 +0800

    first commit

---
+++ QRC20Token.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.4.18;
+import './SafeMath.sol';
+
+
+/**
+    QRC20Token Standard Token implementation
+*/
+contract QRC20Token is SafeMath {
+    string public constant standard = 'Token 0.1';
+    uint256 public constant decimals = 8; // it's recommend to set decimals to 8 in QTUM
+
+
+    // you need change the following three values
+    string public constant name = 'QRC TEST';
+    string public constant symbol = 'QTC';
+    uint256 public totalSupply = 10**9*10**decimals; //you need multi 10^decimals on your real total supply
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function QRC20Token() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    // validates an address - currently only checks that it isn't null
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value)
+    public
+    validAddress(_to)
+    returns (bool success)
+    {
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+    public
+    validAddress(_from)
+    validAddress(_to)
+    returns (bool success)
+    {
+        allowance[_from][msg.sender] = safeSub(allowance[_from][msg.sender], _value);
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+    public
+    validAddress(_spender)
+    returns (bool success)
+    {
+        // if the allowance isn't 0, it can only be updated to 0 to prevent an allowance change immediately after withdrawal
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // disable pay QTUM to this contract
+    function () public payable {
+        revert();
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# QRC20Token
+
+this is an example of QTUM QRC20 Token contract code.
+
+you need change the value of name、symbol and totalSupply in QRC20Token.sol before publish the contract.
+
+
+## Deploy
+
+to deploy the contract, you can refer to [sparknet-guide](https://github.com/qtumproject/qtum/blob/master/doc/sparknet-guide.md)
