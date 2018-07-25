1311de47457f4c543095f469b7e7e51181e4d021
codeface <i@codeface.cn>
2018-07-25 12:18:38 +0800

    add v2

---
+++ QRC20Token_v2.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.18;
+import './SafeMath.sol';
+
+/**
+    QRC20Token Standard Token implementation
+*/
+contract QRC20Token is SafeMath {
+    string public constant standard = 'Token 0.1';
+    string public name = '';
+    string public symbol = '';
+    uint8 public decimals = 0;
+    uint256 public totalSupply = 0;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    //Default assumes totalSupply can't be over max (2^256 - 1).
+    //you need multiply 10^decimals by your real total supply.
+    function QRC20Token(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = totalSupply;
+        Transfer(0x00, msg.sender, totalSupply);
+    }
+
+    // validates an address - currently only checks that it isn't null
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    function name() public constant returns (string name) { name; }
+    function symbol() public constant returns (string symbol) { symbol; }
+    function decimals() public constant returns (uint8 decimals) { decimals; }
+    function totalSupply() public constant returns (uint256 totalSupply) { totalSupply; }
+
+    function transfer(address _to, uint256 _value)
+    public
+    validAddress(_to)
+    returns (bool success)
+    {
+        balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+    public
+    validAddress(_from)
+    validAddress(_to)
+    returns (bool success)
+    {
+        allowance[_from][msg.sender] = safeSub(allowance[_from][msg.sender], _value);
+        balanceOf[_from] = safeSub(balanceOf[_from], _value);
+        balanceOf[_to] = safeAdd(balanceOf[_to], _value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+    public
+    validAddress(_spender)
+    returns (bool success)
+    {
+        // To change the approve amount you first have to reduce the addresses`
+        //  allowance to zero by calling `approve(_spender, 0)` if it is not
+        //  already 0 to mitigate the race condition described here:
+        //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        require(_value == 0 || allowance[msg.sender][_spender] == 0);
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // disable pay QTUM to this contract
+    function () public payable {
+        revert();
+    }
+}
